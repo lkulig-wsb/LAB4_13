6007649e4131db0e1d1578bf3ef756a43aa46e3a
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-01 17:14:32 +0100

    Konstruktora i dane składowe.

--- src/MyPacakage1/Car.java
+++ src/MyPacakage1/Car.java
@@ -1,4 +1,16 @@
 package MyPacakage1;
 
 public class Car {
+    private String marka;
+    private String model;
+    private int rokProdukcji;
+    private int przebieg;
+
+    public Car (String marka, String model, int rokProdukcji, int przebieg)
+    {
+        this.marka = marka;
+        this.model = model;
+        this.rokProdukcji = rokProdukcji;
+        this.przebieg = przebieg;
+    }
 }
