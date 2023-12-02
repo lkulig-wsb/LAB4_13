793adc0783ee2a2f5fc55a59e1fb5031c04923ae
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-02 09:51:06 +0100

    Dodatkowe pola do klasy Car.

--- src/Main.java
+++ src/Main.java
@@ -25,17 +25,18 @@ public class Main {
         Car[] c = new Car[7];
 
         //Inicjalizacja elementów tablicy
-        c[0] = new Car("Ford","C-Max Grand",2011,202150);
-        c[1] = new Car("Mazda", "MX-5",2022, 12000);
-        c[2] = new Car("Toyota","Auris", 2019, 102540);
-        c[3] = new Car("Seat","Leon", 2009, 314000);
-        c[4] = new Car("BMW","M3", 2002, 250678);
-        c[5] = new Car("Fiat","500",2011,250150);
-        c[6] = new Car("Ford","Mondeo",2017,170670);
+        c[0] = new Car("Ford","C-Max Grand",2011,202150,5,1993.0,26000.0);
+        c[1] = new Car("Mazda", "MX-5",2022, 12000,3,1998.0, 150000.0);
+        c[2] = new Car("Toyota","Auris", 2019, 102540,5,1798.0,87000.0);
+        c[3] = new Car("Seat","Leon", 2009, 314000,5,1896.0,32000.0);
+        c[4] = new Car("BMW","M3", 2002, 250678,3,2998.0, 180000.0);
+        c[5] = new Car("Fiat","500",2014,250150, 3, 1242.0,31000.0);
+        c[6] = new Car("Ford","Mondeo",2017,170670, 5,2967.0,28500.0);
 
         //Wyswietlenie informacji o samochodach z tablicy
         for(Car car : c){
-            System.out.println(car.marka + " " + car.model + " " + car.rokProdukcji + " " + car.przebieg);
+            //System.out.println(car.marka + " " + car.model + " " + car.rokProdukcji + " " + car.przebieg);
+            car.wyswietlInfo();
         }
 
     }
--- src/MyPacakage1/Car.java
+++ src/MyPacakage1/Car.java
@@ -5,19 +5,29 @@ public class Car {
     public String model;
     public int rokProdukcji;
     public int przebieg;
+    public int liczbaDrzwi;
+    public double cena;
+    public double pojemnoscSilnika;
 
-    public Car (String marka, String model, int rokProdukcji, int przebieg)
+    public Car (String marka, String model, int rokProdukcji, int przebieg,
+                int liczbaDrzwi, double pojemnoscSilnika, double cena)
     {
         this.marka = marka;
         this.model = model;
         this.rokProdukcji = rokProdukcji;
         this.przebieg = przebieg;
+        this.liczbaDrzwi = liczbaDrzwi;
+        this.pojemnoscSilnika = pojemnoscSilnika;
+        this.cena = cena;
     }
     public void wyswietlInfo()
     {
         System.out.println("Marka:  " + marka);
         System.out.println("Model:  " + model);
-        System.out.println("Rok Produkcji: " + rokProdukcji);
-        System.out.println("Przebieg:   " + przebieg + "\n");
+        System.out.println("Rok Produkcji:  " + rokProdukcji);
+        System.out.println("Przebieg:   " + przebieg);
+        System.out.println("Liczba drzwi:   " + liczbaDrzwi);
+        System.out.println("Pojemność silnika:  " + pojemnoscSilnika);
+        System.out.println("Cena:   " + cena + "\n");
     }
 }
